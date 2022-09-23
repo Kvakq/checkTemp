3e981494445ccbb8f776a1ad7e4abcb6eff21a54
Kvakq <113688353+Kvakq@users.noreply.github.com>
2022-09-23 13:13:26 +0300

    Update checktemp.js

--- checktemp.js
+++ checktemp.js
@@ -6,9 +6,7 @@
 
 const checkTemp = (temp) => {
 const t = Number(temp);
-if (t >= 20) return -1
-if (t <=40 && t >=21  )return 0
+if (t <= 20) return -1
+if (t >=40 && t <=21  )return 0
 return 1;
 }
-
-
